575ab00f0940dc62cfe14b25f6b5c52c1baad4f6
RubyDEAD <reitzdave.andriano@gmail.com>
2024-11-11 16:25:39 +0800

    Update LobbyController.java

--- src/main/java/com/warofoop/warofoop/controllers/LobbyController.java
+++ src/main/java/com/warofoop/warofoop/controllers/LobbyController.java
@@ -1,10 +1,9 @@
-package com.warofoop.warofoop.controllers;
-
 import com.warofoop.warofoop.SceneManager;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-
 import java.io.IOException;
 
 public class LobbyController
