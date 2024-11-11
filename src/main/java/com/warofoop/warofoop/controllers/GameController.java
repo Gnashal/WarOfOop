a1e3a76aa87bfc5c0c38039c995aab7b11502877
Raidiniom <raidiniom22PH@gmail.com>
2024-11-11 16:53:16 +0800

    Health Bar is Working and the zero player health is also working

--- src/main/java/com/warofoop/warofoop/controllers/GameController.java
+++ src/main/java/com/warofoop/warofoop/controllers/GameController.java
@@ -1,11 +1,13 @@
 package com.warofoop.warofoop.controllers;
 
 import com.warofoop.warofoop.SceneManager;
+import com.warofoop.warofoop.build.*;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 
@@ -13,6 +15,7 @@ import java.io.IOException;
 
 public class GameController
 {
+
     @FXML
     AnchorPane gamePane;
 
@@ -31,12 +34,17 @@ public class GameController
     @FXML
     private Label playerLabelEcon2;
 
+    @FXML
+    private ProgressBar playerHealthDisplay1;
+
+    @FXML
+    private ProgressBar playerHealthDisplay2;
+
     public int playerHealth1 = 100;
     public int playerHealth2 = 100;
 
     public int playerEcon1 = 0;
     public int playerEcon2 = 0;
-    public int roundCount = 1;
 
     public void initialize() {
         gamePane.setFocusTraversable(true);
@@ -44,7 +52,27 @@ public class GameController
 
         gamePane.setOnKeyPressed(this::hotKey);
 
+        healthbar(playerHealthDisplay1, playerHealth1);
+        healthbar(playerHealthDisplay2, playerHealth2);
+
         runEcon();
+    }
+
+
+    public void healthbar(ProgressBar bar, int health) {
+        bar.setProgress(health / 100.0);
+
+        String color;
+
+        if (health > 70) {
+            color = "#4caf50";
+        } else if (health > 30) {
+            color = "#ffeb3b";
+        } else {
+            color = "#f44336";
+        }
+
+        bar.setStyle(" -fx-accent: "+ color +";");
     }
 
     @FXML
@@ -63,10 +91,31 @@ public class GameController
             case I -> playerEcon2 -= 10;
             case O -> playerEcon2 -= 20;
             case P -> playerEcon2 -= 15;
+
+            case Z -> playerHealth1 -= 10;
+            case X -> playerHealth2 -= 10;
         }
+
+        playerHealth1 = Math.max(playerHealth1, 0);
+        playerHealth2 = Math.max(playerHealth2, 0);
+
+        healthbar(playerHealthDisplay1, playerHealth1);
+        healthbar(playerHealthDisplay2, playerHealth2);
 
         playerLabelEcon1.setText(" "+ playerEcon1);
         playerLabelEcon2.setText(" "+ playerEcon2);
+
+        if (playerHealth1 <= 0 || playerHealth2 <= 0) {
+            Platform.runLater(() -> {
+                if (playerHealth1 > 0) {
+                    System.out.println(playerName1+ ", Won!");
+                }
+
+                if (playerHealth2 > 0) {
+                    System.out.println(playerName2+ ", Won!");
+                }
+            });
+        }
     }
 
     private void runEcon() {
