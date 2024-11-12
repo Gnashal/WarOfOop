180ae9f8acac50dde81914681eee0243d6d42803
CallenCaracy <diangcocalen@gmail.com>
2024-11-12 21:40:19 +0800

    yo map

--- src/main/java/com/warofoop/warofoop/controllers/LobbyController.java
+++ src/main/java/com/warofoop/warofoop/controllers/LobbyController.java
@@ -1,82 +1,90 @@
 package com.warofoop.warofoop.controllers;
+
 import com.warofoop.warofoop.SceneManager;
+import com.warofoop.warofoop.build.Game;
+import com.warofoop.warofoop.build.Player;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.image.Image;
-import java.io.File;
-import java.net.MalformedURLException;
 import java.io.IOException;
 
-public class LobbyController
-{
-    private final String[] imagePaths = {
-            "/Maps/DebugMap.png", // debug maps
-            "/Maps/DebugMap2.png" // debug maps
-    };
+public class LobbyController {
+
+    private Player player1;
+    private Player player2;
+    private Game game;
+
+    private SceneManager sceneManager;
 
     @FXML
-    private ListView<String> imageListView;
+    private AnchorPane lobbyPane;
 
     @FXML
-    private ImageView imageView;
+    private Button backButton, player1Button, player2Button;
 
     @FXML
-    AnchorPane lobbyPane;
+    private Label statusLabel;
 
     @FXML
-    Button backButton;
+    private TextField playerName1, playerName2;
 
-    @FXML
-    Button player1Button;
+    private boolean player1Ready = false;
+    private boolean player2Ready = false;
 
-    @FXML
-    Button player2Button;
-
-    @FXML
-    Label statusLabel;
-
-    boolean player1Ready = false;
-    boolean player2Ready = false;
-
-
-    public void toggle_player1(javafx.event.ActionEvent actionEvent) throws IOException {
-        if (!player1Ready) {
-            player1Ready = true;
-            player1Button.setText("Unready");
-        } else {
-            player1Ready = false;
-            player1Button.setText("Ready!");
-        }
-        p1p2_ready();
+    public Player getPlayer1() {
+        return player1;
     }
 
-    public void toggle_player2(javafx.event.ActionEvent actionEvent) throws IOException {
-        if (!player2Ready) {
-            player2Ready = true;
-            player2Button.setText("Unready");
-        } else {
-            player2Ready = false;
-            player2Button.setText("Ready!");
-        }
-        p1p2_ready();
+    public Player getPlayer2() {
+        return player2;
     }
 
+    public Game getGame() {
+        return game;
+    }
 
-    public void p1p2_ready() throws IOException {
+    // Set the SceneManager
+    public void setSceneManager(SceneManager sceneManager) {
+        this.sceneManager = sceneManager;
+        System.out.println("SceneManager has been set in LobbyController.");
+    }
+
+    // Check if both players are ready and switch to the game scene
+    private void p1p2_ready() throws IOException {
         if (player1Ready && player2Ready) {
             statusLabel.setText("Both players are ready! Starting the game...");
+            System.out.println("Both players ready, switching to game scene...");
+
+            // Ga-initializeGame kog Player and Game classes here and to be passed to the Game Controller
+            String name1 = playerName1.getText();
+            String name2 = playerName2.getText();
+            player1 = new Player(100f, 500, name1, 10);
+            player2 = new Player(100f, 500, name2, 10);
+            game = new Game(player1, player2);
             goToGame();
         } else {
             updateStatusLabel();
         }
     }
 
+    @FXML
+    public void toggle_player1(ActionEvent event) throws IOException {
+        player1Ready = !player1Ready;
+        player1Button.setText(player1Ready ? "Unready" : "Ready!");
+        System.out.println("Player 1 ready: " + player1Ready);
+        p1p2_ready();
+    }
 
-
+    @FXML
+    public void toggle_player2(ActionEvent event) throws IOException {
+        player2Ready = !player2Ready;
+        player2Button.setText(player2Ready ? "Unready" : "Ready!");
+        System.out.println("Player 2 ready: " + player2Ready);
+        p1p2_ready();
+    }
 
     // Update status label based on which players are ready
     private void updateStatusLabel() {
@@ -88,51 +96,25 @@ public class LobbyController
             statusLabel.setText("Waiting for both players to be ready...");
         }
     }
-    
+
     @FXML
     public void returnToPrevScene() throws IOException {
-        System.out.println("Switched to Main Menu");
-        new SceneManager(lobbyPane, "Main_Menu.fxml");
-    }
-
-    //    for maps
-    @FXML
-    public void loadPreloadedImages() {
-        for (String path : imagePaths) {
-            try {
-                String imagePath = getClass().getResource(path).toExternalForm();
-                imageListView.getItems().add(imagePath.substring(imagePath.lastIndexOf('/') + 1));
-            } catch (NullPointerException e) {
-                System.out.println("Error: Image path not found: " + path);
-            }
+        if (sceneManager == null) {
+            System.out.println("SceneManager is null! Cannot switch to main menu.");
+            return;
         }
+        System.out.println("Switching to Main Menu...");
+        sceneManager.switchToMainMenu();
     }
-
-    @FXML
-    public void handleSelectImage() {
-        String selectedImageName = imageListView.getSelectionModel().getSelectedItem();
-        if (selectedImageName != null) {
-            for (String path : imagePaths) {
-                File file = new File(getClass().getResource(path).getPath());
-                if (file.getName().equals(selectedImageName)) {
-                    try {
-                        Image image = new Image(file.toURI().toURL().toString());
-                        imageView.setImage(image);
-                        System.out.println("Selected image: " + selectedImageName);
-                    } catch (MalformedURLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                }
-            }
-        }
-    }
-
 
     @FXML
     public void goToGame() throws IOException {
-        System.out.println("Entering Game!");
-        new SceneManager(lobbyPane, "Game_Window.fxml");
+        if (sceneManager == null) {
+            System.out.println("SceneManager is null! Cannot switch to game scene.");
+            return;
+        }
+        System.out.println("Switching to Game Scene...");
+        game.startGame();
+        sceneManager.switchToGame(player1, player2, game);
     }
-
 }
